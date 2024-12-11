97cdf2a02cc845af39fadcd07166737224c4cc31
Saadatzi <saadatzi@gmail.com>
2024-12-11 13:49:32 +0100

    chore(package): add redis to both AppHost and Web

--- IoT.Dashboard.AppHost/Program.cs
+++ IoT.Dashboard.AppHost/Program.cs
@@ -2,6 +2,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.IoT_Dashboard_SensorDataAPI>("apiservice");
 
+var redis = builder.AddRedis("redis");
+
 builder.AddProject<Projects.IoT_Dashboard_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(apiService)
--- IoT.Dashboard.Web/Program.cs
+++ IoT.Dashboard.Web/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add service defaults & Aspire client integrations.
 builder.AddServiceDefaults();
 
+builder.AddRedisOutputCache("redis");
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
